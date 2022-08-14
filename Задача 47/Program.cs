ebf4c036d187d3277812830fa1f9ec0dffa9e334
nikolai <nikolai71@msn.com>
2022-08-14 21:12:21 +0300

    first commit

--- Задача 47/Program.cs
+++ Задача 47/Program.cs
@@ -4,18 +4,18 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-namespace CSharp_Shell
-{ public static class Program
-{ public static void Main()
-{ Int32 k, l, m, n;
-Console.Write("m n: ");
-var s = Console.ReadLine()!.Split(" ");
-m = Convert.ToInt32(s[0]);
-n = Convert.ToInt32(s[1]);
-Random rnd = new Random();
-double [,] A = new double [m,n];
-for (k = 0; k < m; k++)
-{ for (l = 0; l < n; l++)
-{ A[k,l]=20.0*rnd.Next()/2147483648-10.0;
-Console.Write(String.Format("{0,5:f1}",
-A[k,l])); } Console.WriteLine(); } } } }
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int [m, n];
+// Random rnd = new Random();
+// double [,] A = new double [m,n];
+for (int i = 0; i < m; i++){
+    for (int j = 0; j < n; j++){
+        matrix[i, j] = new Random().Next(-100, 100);
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine(); 
+}
